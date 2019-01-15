2bd8e4f4e3b60c2304efb88a9ff3b7923d6c694c
Ramiro Estrugo <ramiro@fateware.com>
2019-01-14 22:55:51 -0800

    s/bes_shell/bes/ that got messed up in the process of subtreeing

--- bes_shell.sh
+++ bes_shell.sh
@@ -1,10 +1,10 @@
 #-*- coding:utf-8; mode:shell-script; indent-tabs-mode: nil; sh-basic-offset: 2; tab-width: 2 -*-
 
-function _bes_shell_trace() ( if [[ "$_BES_TRACE" == "1" ]]; then printf '_BES_TRACE: %s\n' "$*"; fi )
-function _bes_shell_trace_function() ( _bes_shell_trace "func: ${FUNCNAME[1]}($*)" )
-function _bes_shell_trace_file() ( _bes_shell_trace "file: ${BASH_SOURCE}: $*" )
+function _bes_trace() ( if [[ "$_BES_TRACE" == "1" ]]; then printf '_BES_TRACE: %s\n' "$*"; fi )
+function _bes_trace_function() ( _bes_trace "func: ${FUNCNAME[1]}($*)" )
+function _bes_trace_file() ( _bes_trace "file: ${BASH_SOURCE}: $*" )
 
-_bes_shell_trace_file "begin"
+_bes_trace_file "begin"
 
 _BES_BASIC_PATH=$(PATH=/bin:/usr/bin env -i bash -c "echo \"${PATH}\"")
 
@@ -12,7 +12,7 @@ _BES_TR_EXE=$(PATH=$_BES_BASIC_PATH which tr)
 _BES_BASENAME_EXE=$(PATH=$_BES_BASIC_PATH which basename)
 
 # return a colon separated path without the head item
-function bes_shell_path_without_head()
+function bes_path_without_head()
 {
   local _path="$*"
   local _head=${_path%%:*}
@@ -25,7 +25,7 @@ function bes_shell_path_without_head()
 }
 
 # return just the head item of a colon separated path
-function bes_shell_path_head()
+function bes_path_head()
 {
   local _path="$*"
   local _head=${_path%%:*}
@@ -34,7 +34,7 @@ function bes_shell_path_head()
 }
 
 # Strip rogue colon(s) from head of path
-function bes_shell_path_head_strip_colon()
+function bes_path_head_strip_colon()
 {
   local _path="$*"
   local _result="${_path#:}"
@@ -47,7 +47,7 @@ function bes_shell_path_head_strip_colon()
 }
 
 # Strip rogue colon(s) from tail of path
-function bes_shell_path_tail_strip_colon()
+function bes_path_tail_strip_colon()
 {
   local _path="$*"
   local _result="${_path%:}"
@@ -60,55 +60,55 @@ function bes_shell_path_tail_strip_colon()
 }
 
 # Strip rogue colon(s) from head and tail of path
-function bes_shell_path_strip_colon()
+function bes_path_strip_colon()
 {
   local _path="$*"
-  local _result=$(bes_shell_path_head_strip_colon "${_path}")
-  echo $(bes_shell_path_tail_strip_colon "${_result}")
+  local _result=$(bes_path_head_strip_colon "${_path}")
+  echo $(bes_path_tail_strip_colon "${_result}")
   return 0
 }
 
 # remove duplicates from a path
 # from https://unix.stackexchange.com/questions/14895/duplicate-entries-in-path-a-problem
-function bes_shell_path_dedup()
+function bes_path_dedup()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# != 1 ]]; then
-    echo "Usage: bes_shell_path_dedup path"
+    echo "Usage: bes_path_dedup path"
     return 1
   fi
   local _tmp="$*"
   local _head
   local _result=""
   while [[ -n "$_tmp" ]]; do
-    _head=$(bes_shell_path_head "${_tmp}")
-    _tmp=$(bes_shell_path_without_head "${_tmp}")
+    _head=$(bes_path_head "${_tmp}")
+    _tmp=$(bes_path_without_head "${_tmp}")
     case ":${_result}:" in
       *":${_head}:"*) :;; # already there
       *) _result="${_result}:${_head}";;
     esac
   done
-  echo $(bes_shell_path_strip_colon "${_result}")
+  echo $(bes_path_strip_colon "${_result}")
   return 0
 }
 
 # sanitize a path by deduping entries and stripping leading or trailing colons
-function bes_shell_path_sanitize()
+function bes_path_sanitize()
 {
   if [[ $# -ne 1 ]]; then
-    echo "Usage: bes_shell_path_sanitize path"
+    echo "Usage: bes_path_sanitize path"
     return 1
   fi
-  local _path=$(bes_shell_path_dedup "$1")
-  echo $(bes_shell_path_strip_colon "${_path}")
+  local _path=$(bes_path_dedup "$1")
+  echo $(bes_path_strip_colon "${_path}")
   return 0
 }
 
-function bes_shell_path_remove()
+function bes_path_remove()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 2 ]]; then
-    echo "Usage: bes_shell_path_remove path p1 p2 ... pN"
+    echo "Usage: bes_path_remove path p1 p2 ... pN"
     return 1
   fi
   local _tmp="$1"
@@ -116,9 +116,9 @@ function bes_shell_path_remove()
   local _head
   local _result=""
   while [[ -n "$_tmp" ]]; do
-    _head=$(bes_shell_path_head "${_tmp}")
-    _tmp=$(bes_shell_path_without_head "${_tmp}")
-    if ! bes_shell_is_in_list "$_head" "$@"; then
+    _head=$(bes_path_head "${_tmp}")
+    _tmp=$(bes_path_without_head "${_tmp}")
+    if ! bes_is_in_list "$_head" "$@"; then
       if [[ -z ${_result} ]]; then
         _result="${_head}"
       else
@@ -130,11 +130,11 @@ function bes_shell_path_remove()
   return 0
 }
 
-function bes_shell_path_append()
+function bes_path_append()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 2 ]]; then
-    echo "Usage: bes_shell_path_append path p1 p2 ... pN"
+    echo "Usage: bes_path_append path p1 p2 ... pN"
     return 1
   fi
   local path="$1"
@@ -143,20 +143,20 @@ function bes_shell_path_append()
   local result="${path}"
   while [[ $# > 0 ]]; do
     what="$1"
-    result=$(bes_shell_path_remove "${result}" "${what}")
+    result=$(bes_path_remove "${result}" "${what}")
     result="${result}":"${what}"
     shift
   done
-  result=$(bes_shell_path_sanitize "${result}")
+  result=$(bes_path_sanitize "${result}")
   echo "${result}"
   return 0
 }
 
-function bes_shell_path_prepend()
+function bes_path_prepend()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 2 ]]; then
-    echo "Usage: bes_shell_path_prepend path p1 p2 ... pN"
+    echo "Usage: bes_path_prepend path p1 p2 ... pN"
     return 1
   fi
   local _path="$1"
@@ -168,97 +168,97 @@ function bes_shell_path_prepend()
     _result="${_what}":"${_result}"
     shift
   done
-  echo $(bes_shell_path_sanitize "${_result}")
+  echo $(bes_path_sanitize "${_result}")
   return 0
 }
 
-function bes_shell_path_print()
+function bes_path_print()
 {
   if [[ $# != 1 ]]; then
-    echo "Usage: bes_shell_path_print path"
+    echo "Usage: bes_path_print path"
     return 1
   fi
   local _tmp="$*"
   local _head
   while [[ -n "$_tmp" ]]; do
-    _head=$(bes_shell_path_head "${_tmp}")
-    _tmp=$(bes_shell_path_without_head "${_tmp}")
+    _head=$(bes_path_head "${_tmp}")
+    _tmp=$(bes_path_without_head "${_tmp}")
     echo ${_head/$HOME/\~}
   done
   return 0
 }
 
-function bes_shell_env_path_sanitize()
+function bes_env_path_sanitize()
 {
-  _bes_shell_trace_function $*
-  local _var_name=$(bes_shell_variable_map $1)
-  local _value=$(bes_shell_var_get $_var_name)
-  local _new_value=$(bes_shell_path_sanitize "$_value")
-  bes_shell_var_set $_var_name "$_new_value"
+  _bes_trace_function $*
+  local _var_name=$(bes_variable_map $1)
+  local _value=$(bes_var_get $_var_name)
+  local _new_value=$(bes_path_sanitize "$_value")
+  bes_var_set $_var_name "$_new_value"
   return 0
 }
 
-function bes_shell_env_path_append()
+function bes_env_path_append()
 {
-  _bes_shell_trace_function $*
-  local _var_name=$(bes_shell_variable_map $1)
+  _bes_trace_function $*
+  local _var_name=$(bes_variable_map $1)
   shift
   local _parts="$@"
-  local _value=$(bes_shell_var_get $_var_name)
-  local _new_value=$(bes_shell_path_append "$_value" "$_parts")
-  bes_shell_var_set $_var_name "$_new_value"
+  local _value=$(bes_var_get $_var_name)
+  local _new_value=$(bes_path_append "$_value" "$_parts")
+  bes_var_set $_var_name "$_new_value"
   export $_var_name
   return 0
 }
 
-function bes_shell_env_path_prepend()
+function bes_env_path_prepend()
 {
-  _bes_shell_trace_function $*
-  local _var_name=$(bes_shell_variable_map $1)
+  _bes_trace_function $*
+  local _var_name=$(bes_variable_map $1)
   shift
   local _parts="$@"
-  local _value=$(bes_shell_var_get $_var_name)
-  local _new_value=$(bes_shell_path_prepend "$_value" "$_parts")
-  bes_shell_var_set $_var_name "$_new_value"
+  local _value=$(bes_var_get $_var_name)
+  local _new_value=$(bes_path_prepend "$_value" "$_parts")
+  bes_var_set $_var_name "$_new_value"
   export $_var_name
   return 0
 }
 
-function bes_shell_env_path_remove()
+function bes_env_path_remove()
 {
-  _bes_shell_trace_function $*
-  local _var_name=$(bes_shell_variable_map $1)
+  _bes_trace_function $*
+  local _var_name=$(bes_variable_map $1)
   shift
   local _parts="$@"
-  local _value=$(bes_shell_var_get $_var_name)
-  local _new_value=$(bes_shell_path_remove "$_value" "$_parts")
-  bes_shell_var_set $_var_name "$_new_value"
+  local _value=$(bes_var_get $_var_name)
+  local _new_value=$(bes_path_remove "$_value" "$_parts")
+  bes_var_set $_var_name "$_new_value"
   export $_var_name
   return 0
 }
 
-function bes_shell_env_path_clear()
+function bes_env_path_clear()
 {
-  _bes_shell_trace_function $*
-  local _var_name=$(bes_shell_variable_map $1)
-  bes_shell_var_set $_var_name ""
+  _bes_trace_function $*
+  local _var_name=$(bes_variable_map $1)
+  bes_var_set $_var_name ""
   export $_var_name
   return 0
 }
 
-function bes_shell_env_path_print()
+function bes_env_path_print()
 {
-  _bes_shell_trace_function $*
-  local _var_name=$(bes_shell_variable_map $1)
-  local _value=$(bes_shell_var_get $_var_name)
-  bes_shell_path_print $_value
+  _bes_trace_function $*
+  local _var_name=$(bes_variable_map $1)
+  local _value=$(bes_var_get $_var_name)
+  bes_path_print $_value
   return 0
 }
 
 # Return system host name.  linux or macos same as bes_shell/system/host.py
-function bes_shell_system()
+function bes_system()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   local _uname_exe=$(PATH=${_BES_BASIC_PATH} which uname)
   local _uname=$(${_uname_exe})
   local _result
@@ -278,11 +278,11 @@ function bes_shell_system()
 }
 
 # Source a shell file if it exists
-function bes_shell_source()
+function bes_source()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 1 ]]; then
-    printf "\nUsage: bes_shell_source filename\n\n"
+    printf "\nUsage: bes_source filename\n\n"
     return 1
   fi
   local _filename=$1
@@ -293,11 +293,11 @@ function bes_shell_source()
   return 1
 }
 
-function bes_shell_invoke()
+function bes_invoke()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 1 ]]; then
-    printf "\nUsage: bes_shell_invoke function\n\n"
+    printf "\nUsage: bes_invoke function\n\n"
     return 1
   fi
   local _function=$1
@@ -310,11 +310,11 @@ function bes_shell_invoke()
 }
 
 # Source all the *.sh files in a dir if it exists and has such files
-function bes_shell_source_dir()
+function bes_source_dir()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 1 ]]; then
-    printf "\nUsage: bes_shell_source_dir dir\n\n"
+    printf "\nUsage: bes_source_dir dir\n\n"
     return 1
   fi
   local _dir=$1
@@ -330,7 +330,7 @@ function bes_shell_source_dir()
 }
 
 # Convert a single argument string to lower case
-function bes_shell_to_lower()
+function bes_to_lower()
 {
   local _result=$( echo "$@" | $_BES_TR_EXE '[:upper:]' '[:lower:]' )
   echo ${_result}
@@ -338,13 +338,13 @@ function bes_shell_to_lower()
 }
 
 # Return an exit code of 0 if the argument is "true."  true is one of: true, 1, t, yes, y
-function bes_shell_is_true()
+function bes_is_true()
 {
   if [[ $# < 1 ]]; then
-    printf "\nUsage: bes_shell_is_true what\n\n"
+    printf "\nUsage: bes_is_true what\n\n"
     return 1
   fi
-  local _what=$(bes_shell_to_lower "$1")
+  local _what=$(bes_to_lower "$1")
   local _rv
   case "${_what}" in
     true|1|t|y|yes)
@@ -358,10 +358,10 @@ function bes_shell_is_true()
 }
 
 # Return 0 if the first argument is in any of the following arguments
-function bes_shell_is_in_list()
+function bes_is_in_list()
 {
   if [[ $# < 1 ]]; then
-    printf "\nUsage: bes_shell_is_in_list what l1 l2 ... lN\n\n"
+    printf "\nUsage: bes_is_in_list what l1 l2 ... lN\n\n"
     return 1
   fi
   local _what="$1"
@@ -377,11 +377,11 @@ function bes_shell_is_in_list()
   return 1
 }
 
-function bes_shell_setup()
+function bes_setup()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 1 ]]; then
-    printf "\nUsage: bes_shell_setup root_dir [go_there]\n\n"
+    printf "\nUsage: bes_setup root_dir [go_there]\n\n"
     return 1
   fi
   local _root_dir=$1
@@ -390,71 +390,71 @@ function bes_shell_setup()
     _go_there=$2
   fi
 
-  bes_shell_env_path_prepend PATH ${_root_dir}/bin
-  bes_shell_env_path_prepend PYTHONPATH ${_root_dir}/lib
+  bes_env_path_prepend PATH ${_root_dir}/bin
+  bes_env_path_prepend PYTHONPATH ${_root_dir}/lib
 
-  if $(bes_shell_is_true $_go_there); then
+  if $(bes_is_true $_go_there); then
     cd $_root_dir
-    bes_shell_tab_title $($_BES_BASENAME_EXE $_root_dir)
+    bes_tab_title $($_BES_BASENAME_EXE $_root_dir)
   fi
   
   return 0
 }
 
-function bes_shell_unsetup()
+function bes_unsetup()
 {
-  _bes_shell_trace_function $*
+  _bes_trace_function $*
   if [[ $# < 1 ]]; then
-    printf "\nUsage: bes_shell_unsetup root_dir\n\n"
+    printf "\nUsage: bes_unsetup root_dir\n\n"
     return 1
   fi
   local _root_dir=$1
-  bes_shell_env_path_remove PATH ${_root_dir}/bin
-  bes_shell_env_path_remove PYTHONPATH ${_root_dir}/lib
-  bes_shell_tab_title ""
+  bes_env_path_remove PATH ${_root_dir}/bin
+  bes_env_path_remove PYTHONPATH ${_root_dir}/lib
+  bes_tab_title ""
   return 0
 }
 
 # Get a var value
-function bes_shell_var_get()
+function bes_var_get()
 {
   eval 'printf "%s\n" "${'"$1"'}"'
 }
 
 # Set a var value
-function bes_shell_var_set()
+function bes_var_set()
 {
   eval "$1=\"\$2\""
 }
 
 # Unset a var value
-function bes_shell_var_unset()
+function bes_var_unset()
 {
   eval "unset $1"
 }
 
 # Export a var value
-function bes_shell_var_export()
+function bes_var_export()
 {
   eval "export $1=\"\$2\""
 }
 
-function bes_shell_PATH()
+function bes_PATH()
 {
-  bes_shell_env_path_print PATH
+  bes_env_path_print PATH
 }
 
-function bes_shell_PYTHONPATH()
+function bes_PYTHONPATH()
 {
-  bes_shell_env_path_print PYTHONPATH
+  bes_env_path_print PYTHONPATH
 }
 
-function bes_shell_LD_LIBRARY_PATH()
+function bes_LD_LIBRARY_PATH()
 {
-  bes_shell_env_path_print LD_LIBRARY_PATH
+  bes_env_path_print LD_LIBRARY_PATH
 }
 
-function _bes_shell_variable_map_macos()
+function _bes_variable_map_macos()
 {
   local _var_name=$1
   local _rv
@@ -470,7 +470,7 @@ function _bes_shell_variable_map_macos()
   return 0
 }
 
-function _bes_shell_variable_map_linux()
+function _bes_variable_map_linux()
 {
   local _var_name=$1
   local _rv
@@ -486,21 +486,21 @@ function _bes_shell_variable_map_linux()
   return 0
 }
 
-function bes_shell_variable_map()
+function bes_variable_map()
 {
   if [[ $# < 1 ]]; then
-    echo "Usage: bes_shell_variable_map var_name"
+    echo "Usage: bes_variable_map var_name"
     return 1
   fi
-  local _system=$(bes_shell_system)
+  local _system=$(bes_system)
   local _var_name=$1
   local _rv
   case ${_system} in
     macos)
-      _rv=$(_bes_shell_variable_map_macos ${_var_name})
+      _rv=$(_bes_variable_map_macos ${_var_name})
       ;;
     linux|*)
-      _rv=$(_bes_shell_variable_map_linux ${_var_name})
+      _rv=$(_bes_variable_map_linux ${_var_name})
       ;;
   esac
   echo ${_rv}
@@ -509,7 +509,7 @@ function bes_shell_variable_map()
 
 function LD_LIBRARY_PATH_var_name()
 {
-  local _system=$(bes_shell_system)
+  local _system=$(bes_system)
   local _rv=
   case ${_system} in
     macos)
@@ -523,7 +523,7 @@ function LD_LIBRARY_PATH_var_name()
   return 0
 }
 
-function bes_shell_tab_title()
+function bes_tab_title()
 {
   echo -ne "\033]0;"$*"\007"
   local _prompt=$(echo -ne "\033]0;"$*"\007")
@@ -533,7 +533,7 @@ function bes_shell_tab_title()
 # Mostly borrowed from https://unwiredcouch.com/2016/04/13/bash-unit-testing-101.html
 
 # Print all the unit tests defined in this script environment (functions starting with test_)
-function bes_shell_testing_print_unit_tests()
+function bes_testing_print_unit_tests()
 {
   local _result
   declare -a _result
@@ -546,22 +546,22 @@ function bes_shell_testing_print_unit_tests()
   return 0
 }
 
-_bes_shell_testing_exit_code=0
+_bes_testing_exit_code=0
 
 # Run all the unit tests found in this script environment
-function bes_shell_testing_run_unit_tests()
+function bes_testing_run_unit_tests()
 {
-  local _tests=$(bes_shell_testing_print_unit_tests)
+  local _tests=$(bes_testing_print_unit_tests)
   local _test
   local _rv
   for _test in $_tests; do
     ${_test}
   done
-  exit $_bes_shell_testing_exit_code
+  exit $_bes_testing_exit_code
 }
 
 # Run that an expression argument is true and print that
-function bes_shell_assert()
+function bes_assert()
 {
   local _filename=$($_BES_BASENAME_EXE ${BASH_SOURCE[1]})
   local _line=${BASH_LINENO[0]}
@@ -570,10 +570,10 @@ function bes_shell_assert()
   eval "${1}"
   if [[ $? -ne 0 ]]; then
     echo "failed: " ${1} " at $_filename:$_line"
-    _bes_shell_testing_exit_code=1
+    _bes_testing_exit_code=1
   else
     echo "$_filename $_function: passed"
   fi
 }
 
-_bes_shell_trace_file "end"
+_bes_trace_file "end"
