9b2d3984787c08e25c5ff2505f7ebc2c872611ee
Ramiro Estrugo <restrugo@imvu.com>
2020-08-10 10:01:03 -0700

    add bes_question_yes_no()

--- bash/bes_shell/bes_shell.sh
+++ bash/bes_shell/bes_shell.sh
@@ -1182,4 +1182,34 @@ function bes_str_remove_tail()
   return 0
 }
 
+function bes_question_yes_no()
+{
+  if [[ $# != 2 ]]; then
+    echo "usage: _question_yes_no var_name message"
+    return 1
+  fi
+  local _CHOICES="[y]es [n]o"
+  local _var_name="${1}"
+  local _message="${2}"
+  local _local_answer
+  local _result=1
+  while true; do
+    read -p "${_message} - ${_CHOICES}: " _local_answer
+    case "${_local_answer}" in
+      y|Y|yes|YES)
+        _result=yes
+        break
+        ;;
+      n|N|no|NO)
+        _result=no
+        break
+        ;;
+      *)
+        bes_message "Invalid answer: ${_local_answer}.  Please answer: ${_CHOICES}"
+    esac
+  done
+  eval ${_var_name}=${_result}
+  return 0
+}
+
 _bes_trace_file "end"
