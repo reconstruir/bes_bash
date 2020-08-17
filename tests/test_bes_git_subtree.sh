f70975551542934218aa7a38c212e01b2b100da3
Ramiro Estrugo <restrugo@imvu.com>
2020-08-16 21:11:10 -0700

    add unit tests that prove that both specific tag and @latest@ work

--- tests/test_bes_git_subtree.sh
+++ tests/test_bes_git_subtree.sh
@@ -49,4 +49,68 @@ function test_bes_git_subtree_basic()
   rm -rf ${_tmp_src_repo} ${_tmp_dst_repo}
 }
 
+function test_bes_git_subtree_with_revision()
+{
+  local _tmp_src_repo=$(_bes_git_make_temp_repo test_bes_git_subtree_basic1)
+  local _src=${_tmp_src_repo}/local
+
+  local _tmp_dst_repo=$(_bes_git_make_temp_repo test_bes_git_subtree_basic2)
+  local _dst=${_tmp_dst_repo}/local
+
+  _bes_git_add_file "${_src}" "foo/bar/kiwi.txt" kiwi.txt true
+  bes_git_tag "${_src}" "1.2.3"
+  _bes_git_add_file "${_src}" "foo/bar/lemon.txt" lemon.txt true
+  bes_git_tag "${_src}" "1.2.4"
+
+  _bes_git_add_file "${_dst}" "something/apple.txt" apple.txt true
+
+  bes_git_subtree_update \
+    "${_dst}" \
+    master \
+    ${_tmp_src_repo}/remote \
+    master \
+    "1.2.3" \
+    "foo/bar" \
+    "subtree" \
+    false
+  bes_assert "[[ $(bes_testing_call_function test -f ${_dst}/something/apple.txt) == 0 ]]"
+  bes_assert "[[ $(bes_testing_call_function test -f ${_dst}/subtree/kiwi.txt) == 0 ]]"
+  bes_assert "[[ $(bes_testing_call_function test -f ${_dst}/subtree/lemon.txt) == 1 ]]"
+  
+  rm -rf ${_tmp_src_repo} ${_tmp_dst_repo}
+}
+
+function test_bes_git_subtree_with_latest()
+{
+  local _tmp_src_repo=$(_bes_git_make_temp_repo test_bes_git_subtree_basic1)
+  local _src=${_tmp_src_repo}/local
+
+  local _tmp_dst_repo=$(_bes_git_make_temp_repo test_bes_git_subtree_basic2)
+  local _dst=${_tmp_dst_repo}/local
+
+  _bes_git_add_file "${_src}" "foo/bar/kiwi.txt" kiwi.txt true
+  bes_git_tag "${_src}" "1.2.3"
+  _bes_git_add_file "${_src}" "foo/bar/lemon.txt" lemon.txt true
+  bes_git_tag "${_src}" "1.2.4"
+  _bes_git_add_file "${_src}" "foo/bar/watermelon.txt" watermelon.txt false
+
+  _bes_git_add_file "${_dst}" "something/apple.txt" apple.txt true
+
+  bes_git_subtree_update \
+    "${_dst}" \
+    master \
+    ${_tmp_src_repo}/remote \
+    master \
+    "@latest@" \
+    "foo/bar" \
+    "subtree" \
+    false
+  bes_assert "[[ $(bes_testing_call_function test -f ${_dst}/something/apple.txt) == 0 ]]"
+  bes_assert "[[ $(bes_testing_call_function test -f ${_dst}/subtree/kiwi.txt) == 0 ]]"
+  bes_assert "[[ $(bes_testing_call_function test -f ${_dst}/subtree/lemon.txt) == 0 ]]"
+  bes_assert "[[ $(bes_testing_call_function test -f ${_dst}/subtree/watermelon.txt) == 1 ]]"
+  
+  rm -rf ${_tmp_src_repo} ${_tmp_dst_repo}
+}
+
 bes_testing_run_unit_tests
