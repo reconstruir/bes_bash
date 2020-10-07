2d382b9e0cc3646ea88daef495e749654777585a
Ramiro Estrugo <restrugo@imvu.com>
2020-10-06 22:55:38 -0700

    add a helper to make the fake python exes

--- tests/test_bes_python.sh
+++ tests/test_bes_python.sh
@@ -16,6 +16,27 @@ function _test_bes_python_this_dir()
 source "$(_test_bes_python_this_dir)"/../bash/bes_shell/bes_shell.sh
 source "$(_test_bes_python_this_dir)"/../bash/bes_shell/bes_python.sh
 
+function _python_testing_make_testing_python_exe()
+{
+  if [[ $# != 3 ]]; then
+    bes_message "Usage: _python_testing_make_testing_python_exe where basename full_version"
+    return 1
+  fi
+  local _where="${1}"
+  local _basename="${2}"
+  local _full_version="${3}"
+  mkdir -p ${_where}
+  local _exe="${_where}/${_basename}"
+  cat > "${_exe}" << EOF
+#!/bin/bash
+echo Python ${_full_version} 1>&2
+exit 0
+EOF
+  chmod 755 ${_exe}
+  echo "${_exe}"
+  return 0
+}
+
 function test_bes_has_python()
 {
   bes_assert "[[ $(bes_testing_call_function bes_has_python bad ) == 1 ]]"
@@ -24,14 +45,7 @@ function test_bes_has_python()
 function test_bes_python_exe_full_version()
 {
   local _tmp=/tmp/test_bes_python_exe_full_version_$$
-  mkdir -p ${_tmp}
-  local _fake_python=${_tmp}/fake_python.sh
-  cat > ${_fake_python} << EOF
-#!/bin/bash
-echo Python 2.7.666 1>&2
-exit 0
-EOF
-  chmod 755 ${_fake_python}
+  local _fake_python="$(_python_testing_make_testing_python_exe "${_tmp}" fake_python.sh 2.7.666)"
 
   bes_assert "[[ $(bes_python_exe_full_version ${_fake_python}) == 2.7.666 ]]"
 
@@ -41,15 +55,7 @@ EOF
 function test_bes_python_exe_version()
 {
   local _tmp=/tmp/test_bes_python_exe_version_$$
-  mkdir -p ${_tmp}
-  local _fake_python=${_tmp}/fake_python.sh
-  cat > ${_fake_python} << EOF
-#!/bin/bash
-echo Python 2.7.666 1>&2
-exit 0
-EOF
-  chmod 755 ${_fake_python}
-
+  local _fake_python="$(_python_testing_make_testing_python_exe "${_tmp}" fake_python.sh 2.7.666)"
   bes_assert "[[ $(bes_python_exe_version ${_fake_python}) == 2.7 ]]"
 
   rm -rf ${_tmp}
@@ -61,15 +67,8 @@ function test__bes_python_macos_is_builtin()
     return 0
   fi
   local _tmp=/tmp/test__bes_python_macos_is_builtin_$$
-  mkdir -p ${_tmp}
-  local _fake_python=${_tmp}/fake_python.sh
-  cat > ${_fake_python} << EOF
-#!/bin/bash
-echo Python 2.7.666 1>&2
-exit 0
-EOF
-  chmod 755 ${_fake_python}
-
+  local _fake_python="$(_python_testing_make_testing_python_exe "${_tmp}" fake_python.sh 2.7.666)"
+  
   bes_assert "[[ $(bes_testing_call_function _bes_python_macos_is_builtin /usr/bin/python ) == 0 ]]"
   bes_assert "[[ $(bes_testing_call_function _bes_python_macos_is_builtin /usr/bin/python2.7 ) == 0 ]]"
   bes_assert "[[ $(bes_testing_call_function _bes_python_macos_is_builtin ${_fake_python} ) == 1 ]]"
